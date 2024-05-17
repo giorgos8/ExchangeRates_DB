7f21aee04acc79354178acb63fcf804787ab2f56
Giorgos Kokkinos <giorgos.kokkinos@gmail.com>
2024-05-17 15:01:32 +0300

    Rename Table Name of Traces

--- dbo/Functions/SFN_GET_NEXT_AA.sql
+++ dbo/Functions/SFN_GET_NEXT_AA.sql
@@ -7,7 +7,7 @@ RETURNS INT AS
 BEGIN 
 	DECLARE @RET_RUN_ID int
 
-	SELECT @RET_RUN_ID = (SELECT ISNULL(MAX([trc_run_id]), 0) + 1 FROM [dbo].[tbl_traces])
+	SELECT @RET_RUN_ID = (SELECT ISNULL(MAX([trc_run_id]), 0) + 1 FROM [dbo].[traces])
 
 	RETURN @RET_RUN_ID
 END
