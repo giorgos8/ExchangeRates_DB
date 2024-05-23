3a93d5156351844874fe686c34ef246162fa2286
Giorgos Kokkinos <giorgos.kokkinos@gmail.com>
2024-05-23 17:56:57 +0300

    .

--- dbo/Views/VW_EXCHANGE_RATES_USD_MISSING_DATES.sql
+++ dbo/Views/VW_EXCHANGE_RATES_USD_MISSING_DATES.sql
@@ -1,16 +1,16 @@
 ﻿
 
-CREATE view [dbo].[VW_EXCHANGE_RATES_USD_MISSING_DATES]
+CREATE VIEW [dbo].[VW_EXCHANGE_RATES_USD_MISSING_DATES]
 AS
-select
-	cast(c.Date as varchar(10)) as DATE,
-	cast(isnull(r.FROM_CURRENCY, '-') as varchar(10)) as FROM_CURRENCY,
-	cast(isnull(r.TO_CURRENCY, '-') as varchar(10)) as TO_CURRENCY,
-	isnull(cast(r.EXCHANGE_RATE as varchar(20)), '-') as EXCHANGE_RATE
-from [dbo].[Calendar] c
-left join [dbo].[VW_EXCHANGE_RATES_USD] r
-	on c.Date = r.DATE
+SELECT
+	CAST(c.DATE AS VARCHAR(10)) AS DATE,
+	CAST(ISNULL(r.FROM_CURRENCY, '-') AS VARCHAR(10)) as FROM_CURRENCY,
+	CAST(ISNULL(r.TO_CURRENCY, '-') AS VARCHAR(10)) as TO_CURRENCY,
+	ISNULL(CAST(r.EXCHANGE_RATE AS VARCHAR(20)), '-') as EXCHANGE_RATE,
+	ISNULL(CAST(r.INPUT_DATE AS VARCHAR(100)), '-') AS INPUT_DATE
+FROM [dbo].[Calendar] c
+LEFT JOIN [dbo].[VW_EXCHANGE_RATES_USD] r
+	ON c.DATE = r.DATE
 	AND R.FROM_CURRENCY = 'USD'
-where
-	c.date <= getdate()
---order by date desc
+WHERE
+	c.DATE <= GETDATE()
